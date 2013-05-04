4f9b70c18e4626c605187a7cab71e2511be96e9d
Siqi LIU <me@siqi.fr>
2013-05-04 20:12:02 +0200

    sharp

---
+++ arduino/others_gros/driver/sharp.cpp
@@ -0,0 +1,42 @@
+/**
+ *Filename	:sharp.cpp
+ *
+ *Author	:YANG Qiao
+ *Date		:09/03/2013
+ */
+
+#include "math.h"
+#include "sharp.h"
+#include "Arduino.h"
+#include "protocole.h"
+#include "message.h"
+
+void check_sharp(){
+	int sensor_value[4];
+
+	sensor_value[0] = analogRead(PIN_SHARP_ARRIERE_DROIT);
+	sensor_value[1] = analogRead(PIN_SHARP_ARRIERE_GAUCHE);
+	sensor_value[2] = analogRead(PIN_SHARP_AVANT_GAUCHE);
+	sensor_value[3] = analogRead(PIN_SHARP_AVANT_DROIT);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (sensor_value[i] > THRESHOLD_PROCHE)
+            sendEvent(Q_SHARP_TROP_PROCHE, i+1);
+	}
+}
+
+/**
+ * Convert analog output into cm.
+ * Not used because dependent on model.
+ */
+// float sharp_distance(){
+// 	sharp_init();
+// 	float distance=0;
+
+// 	int sensor_value=analogRead(PIN_SHARP_ARRIERE_DROIT);
+
+// 	distance=2020*pow(sensor_value,-1);
+
+// 	return distance;
+// }
---
+++ arduino/others_gros/driver/sharp.h
@@ -0,0 +1,13 @@
+#ifndef __SHARP_H
+#define __SHARP_H 
+
+#define PIN_SHARP_AVANT_GAUCHE 2
+#define PIN_SHARP_AVANT_DROIT 3
+#define PIN_SHARP_ARRIERE_GAUCHE 1
+#define PIN_SHARP_ARRIERE_DROIT 0
+
+#define THRESHOLD_PROCHE 300
+
+void check_sharp();
+
+#endif
